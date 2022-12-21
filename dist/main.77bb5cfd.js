04551e8555abea33f91ff8b158da8dac0900d440
Wadie Benabdouh <wadie.trade@gmail.com>
2022-12-21 03:21:15 +0100

    early access done

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"C:\\Users\\wadie\\Desktop\\fylo-landing-page\\src\\assets\\bg-curve-mobile.svg":[["bg-curve-mobile.9caaa28e.svg","assets/bg-curve-mobile.svg"],"assets/bg-curve-mobile.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\wadie\\Desktop\\fylo-landing-page\\src\\assets\\bg-curve-mobile.svg":[["bg-curve-mobile.9caaa28e.svg","assets/bg-curve-mobile.svg"],"assets/bg-curve-mobile.svg"],"C:\\Users\\wadie\\Desktop\\fylo-landing-page\\src\\assets\\bg-curve-desktop.svg":[["bg-curve-desktop.71da881b.svg","assets/bg-curve-desktop.svg"],"assets/bg-curve-desktop.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
